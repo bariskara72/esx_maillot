d3594e2001b71b307685cb13dfb6ccb75f1afccd
Barış <64877968+bariskara72@users.noreply.github.com>
2020-09-14 00:31:56 +0300

    Add files via upload

---
+++ esx_maillot/locales/fr.lua
@@ -0,0 +1,7 @@
+Locales['fr'] = {
+
+	['access_jail_center'] = 'Plaj kabinlerine erişmek için E düğmesine basın.',
+	['maillot_center'] = 'Plaj kabini',
+	['maillot_wear'] = 'Mayo',
+	['citizen_wear'] = 'Sivil Kıyafet',
+}
